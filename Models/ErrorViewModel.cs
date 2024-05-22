4044dfd0c2f05dd663c6981941c7347ee8d5e348
Arjun Pawar <Arjun Pawar@DESKTOP-EIPHC61>
2024-05-22 11:10:55 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebSphere_Innovations.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
